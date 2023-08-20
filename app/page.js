456dfe06522362c2dedce8b697ab30579b8f9159
Jesse Eisenbart <jesseeisenbart@gmail.com>
2023-08-19 21:51:24 -0600

    BIG UPDATE

--- app/page.js
+++ app/page.js
@@ -1,113 +1,54 @@
 import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faThreads } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faTiktok } from '@fortawesome/free-brands-svg-icons'
+
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between px-6 lg:px-12 2xl:px-20">
+			<div className="z-20 bg-gradient-to-t from-clay from-10% via-blue via-20% to-blue/50 w-full h-full fixed animate-fade"/>
+			<div className="z-10 w-full h-full fixed bottom-20 lg:bottom-0">
+				<Image
+					src="/banner.gif"
+					alt="Blood Running preview"
+					layout='fill'
+					objectFit='cover'
+					priority
+				/>
+			</div>
+			<div className="z-30 w-full text-sm animate-slide mt-52">
+				<h1 className='z-30 font-aware text-4xl lg:text-8xl text-white border-b-8 border-pink pt-24 pb-2 w-fit'>
+					BLOOD
+					<br/>
+					RUNNING
+				</h1>
+				<p className='text-lg lg:text-xl max-w-prose font-anonymous text-white object-contain my-12'>
+					Make runs through the cyber-dystopian wasteland, plunder what you can, and bleed to <strong className='text-pink'>SURVIVE</strong>.
+					<br/>
+					<br/>
+					Join our <u><a href='#' className='cursor-pointer'>mailing list</a></u> and be the first hear news about
+					<br/>
+					<strong className='text-pink'>BLOOD RUNNING</strong>.
+					<br/>
+				</p>
+				<button className="transition-colors duration-100 text-lg lg:text-2xl rounded-none font-anonymous object-contain bg-pink h-14 px-14 w-full lg:w-auto lg:max-w-xs text-clay hover:bg-white focus:shadow-outline"><strong>JOIN NOW</strong></button>
+			</div>
+			<footer className="z-30 w-full text-sm my-12 animate-fade">
+				<ul className='font-anonymous text-white flex flex-row space-x-3 lg:space-x-4 content-center'>
+					<li className='text-s lg:text-l h-6 mr-auto'>© 2023 DEAD UNICORN INC.</li>
+					<li><a href='https://discord.gg/X5kdyhAXVu' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faDiscord} className='h-6 w-auto'/></a></li>
+					<li><a href='https://twitter.com/DeadUnicornInc' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faXTwitter} className='h-6 w-auto'/></a></li>
+					<li><a href='https://www.instagram.com/deadunicorninc/' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faInstagram} className='h-6 w-auto'/></a></li>
+					<li><a href='https://www.threads.net/deadunicorninc/' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faThreads} className='h-6 w-auto'/></a></li>
+					<li><a href='https://www.youtube.com/c/DeadUnicorn' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faYoutube} className='h-6 w-auto'/></a></li>
+					<li><a href='https://www.tiktok.com/@deadunicorninc' rel='noreferrer' target='_blank'><FontAwesomeIcon icon={faTiktok} className='h-6 w-auto'/></a></li>
+				</ul>
+			</footer>
+		</main>
+	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,72 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				pink: '#ff4357',
+				blue: '#0d0e16',
+				clay: '#04060f',
+				transparent: 'transparent',
+				current: 'currentColor',
+				black: colors.black,
+				white: colors.white,
+				emerald: colors.emerald,
+				indigo: colors.indigo,
+				yellow: colors.yellow,
+				stone: colors.warmGray,
+				sky: colors.lightBlue,
+				neutral: colors.trueGray,
+				gray: colors.coolGray,
+				slate: colors.blueGray,
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			fontFamily: {
+				aware: 'Aware',
+				anonymous: 'Anonymous',
+				anonymousBold: 'AnonymousBold',
+			},
+			keyframes: {
+				slide: {
+					'0%': { marginRight: '50%', opacity: '0' },
+					'100%': { marginRight: '0%', opacity: '1' },
+				},
+				fade: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				}
+			},
+			animation: {
+				slide: 'slide 0.8s cubic-bezier(0.8, 0.6, 0.8, 1)',
+				fade: 'fade 1s ease-in-out',
+			},
+			screens: {
+				'sm': '640px',
+				// => @media (min-width: 640px) { ... }
+		
+				'md': '768px',
+				// => @media (min-width: 768px) { ... }
+		
+				'lg': '1024px',
+				// => @media (min-width: 1024px) { ... }
+		
+				'xl': '1280px',
+				// => @media (min-width: 1280px) { ... }
+
+				'2xl': '2000px',
+				// => @media (min-width: 1280px) { ... }
+			}  
+		},
+	},
+	plugins: [],
 }
