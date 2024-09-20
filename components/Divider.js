1c70fc5efca927f188247cc05a7ae533db6a7a60
Jesse Eisenbart <jesseeisenbart@gmail.com>
2024-09-20 16:49:54 -0600

    All changes

--- components/Divider.js
+++ components/Divider.js
@@ -1,24 +1,30 @@
-import Image from 'next/image'
-import { motion } from 'framer-motion'
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Divider = () => {
-    return (
-        <motion.div
-            className='w-full h-full flex justify-center my-32'
-            initial={{ opacity: 0, y: 60 }}
-            whileInView={{ opacity: 1, y: 0, transition: { duration: 0.5, ease: 'easeInOut' }}}
-            viewport={{ once: true }}
-        >
-            <Image 
-                className='px-16'
-                src="/images/divider.png"                 
-                width={0}
-                height={0}
-                style={{ width: '100%', height: 'auto' }} // optional
-                alt="logo" 
-            />
-        </motion.div>
-    )
-}
+	return (
+		<motion.div
+			className='w-full h-full flex justify-center my-32'
+			initial={{ opacity: 0, y: 60 }}
+			whileInView={{
+				opacity: 1,
+				y: 0,
+				transition: { duration: 0.5, ease: 'easeInOut' },
+			}}
+			viewport={{ once: true }}
+		>
+			<Image
+				className='px-16'
+				src='/images/divider.png'
+				priority
+				quality={100}
+				width={0}
+				height={0}
+				style={{ width: '100%', height: 'auto' }} // optional
+				alt='logo'
+			/>
+		</motion.div>
+	);
+};
 
-export default Divider
+export default Divider;
